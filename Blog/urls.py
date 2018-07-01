0190cd561aff3177a677cac2200825d598085afe
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-02 01:01:04 +0200

    implement bootstrap

--- Blog/urls.py
+++ Blog/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf.urls.static import static
 
 # from posts import views as post_view
 
@@ -23,3 +25,6 @@ urlpatterns = [
     # path('posts/$', post_home, name='post_home'),
     re_path(r'^posts/', include(('posts.urls', 'posts') , namespace='posts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
