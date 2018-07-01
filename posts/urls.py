a1d9928af7ae8169e4de9962544ae4d50c5704f0
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-01 22:31:09 +0200

    dynamic urls

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.post_list),
     re_path(r'^create/$', views.post_create),
-    re_path(r'^detail/$', views.post_detail),
+    re_path(r'^(?P<id>\d+)/$', views.post_detail),
     re_path(r'^update/$', views.post_update),
     re_path(r'^delete/$', views.post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Post
@@ -8,8 +8,13 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
-	return HttpResponse("<h1>Detail</h1>")
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id )
+	context = {
+		"title": instance.title,
+		"instance": instance
+	}
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
 	queryset = Post.objects.all()
