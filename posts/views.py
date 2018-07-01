8926588298090510935cdc0d93dacc377dc6c33b
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-01 22:20:13 +0200

    Render templates

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Post
 
 
 def post_create(request):
@@ -12,7 +12,12 @@ def post_detail(request):
 	return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request):
-	return HttpResponse("<h1>List</h1>")
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset,
+		"title": "List"
+	}
+	return render(request, "index.html", context)
 
 def post_update(request):
 	return HttpResponse("<h1>Update</h1>")
