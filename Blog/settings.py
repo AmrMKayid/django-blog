a081478359a3134334cba4122998170590c25ce4
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-05 18:43:22 +0200

    Slugs and Tags

--- Blog/settings.py
+++ Blog/settings.py
@@ -131,3 +131,4 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
+ALLOW_UNICODE_SLUGS = True
--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.post_list, name='list'),
     re_path(r'^create/$', views.post_create),
-    re_path(r'^(?P<id>\d+)/$', views.post_detail, name='detail'),
-    re_path(r'^(?P<id>\d+)/edit/$', views.post_update, name='update'),
-    re_path(r'^(?P<id>\d+)/delete/$', views.post_delete),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
+    re_path(r'^(?P<slug>[\w-]+)/edit/$', views.post_update, name='update'),
+    re_path(r'^(?P<slug>[\w-]+)/delete/$', views.post_delete),
 ]
